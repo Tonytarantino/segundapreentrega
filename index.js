02b5519836aac5320674a8002806b2e0e4e7226e
anto paoletti <antonelapaoletti@gmail.com>
2022-09-25 01:51:08 -0300

    funciona storage

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let titulo = document.getElementById("tituloPortada");
 let subtitulo = document.getElementById("subtituloPortada");
 let parrafoInscripciones = document.getElementById("parrafoInscripciones");
+let portada = document.getElementById("portada");
+/* let nombreEstudiante = document.getElementById("nombreRespuesta"); */
+/* let nombreResultado = document.getElementById("nombreRespuesta"); */
+
+/* let fichaInscripcion = document.querySelector(".fichaInscripcion")  */
+
 
 
 //microbit
@@ -63,3 +69,59 @@ tarjetaMicrobit.addEventListener("click", ()=>){
   document.formulario.append(formularioInscripcion);
 });
  */
+
+
+/* let nombreEstudianteIngresado = document.getElementById("nombreRegistro").value */
+/* 
+let nombreEstudianteIngresado = document.setElementById("nombreRegistro").value;
+let nombreEstudianteRegistrado = localStorage.getItem("nombreEstudiante");
+console.log(nombreEstudiante);
+let nombreRespuesta = document.getElementById("nombreEstudianteRegistrado ");
+let boton = document.getElementById("boton");
+boton.addEventListener("click", () => {
+  console.log("nombreEstudinteRegistrado",nombreEstudinteRegistrado );
+  nombreRespuesta.innerHTML = nombreEstudianteIngresado  ;
+  cards.remove();
+  formReg.remove();
+}); */
+
+window.onload = iniciar;
+
+function iniciar() {
+
+  let btnInscribirme = document.getElementById("inscribirme");
+  btnInscribirme.addEventListener("click", clickInscribirme);
+  event.preventDefault();
+
+}
+
+function clickInscribirme() {
+
+  let nombreIngresado = document.getElementById("nombreEstudiante"); //mete el nombre en una variable
+  let estudiante = []; //inventa un arreglo con esa variable
+  estudiante.push(nombreIngresado.value); //ingresa datos en el arreglo
+  localStorage.estudiante = JSON.stringify(estudiante); //mete variable a storage y lo convierte en string
+  localStorage.nombreEstudiante = nombreEstudiante.value;
+  portada.remove();
+
+
+  
+  let fichaInscripcion = document.getElementById("fichaInscripcion");
+  fichaInscripcion.innerHTML = 
+  `<h2> Ficha de inscripcion</h2>
+   <h4>Nombre de estudiante: ${estudiante} </h4>`;
+  mostrarFicha();
+}
+
+function mostrarFicha() {
+  let estudiante = JSON.parse(localStorage.estudiante); //texto
+
+  //let respuestaNombre = document.getElementById("respuesta"); SIRVE
+  //respuestaNombre.innerHTML =  localStorage.nombreEstudiante ; SIRVE
+
+  /* let fichaInscripcion = document.getElementById(fichaInscripcion);
+  fichaInscripcion.innerHTML = ` <h4>Nombre de estudiante: ${nombreIngresado} </h4>`; */ --> //quizas no va acá
+  alert(localStorage.nombreEstudiante);
+  click.preventDefault();
+
+}
